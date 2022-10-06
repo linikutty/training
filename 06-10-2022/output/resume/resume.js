efcb5333523c66cb45850a48138831cf89053fd3
unknown <alfiyashalini@gmail.com>
2022-10-06 17:02:33 +0530

    resume task

---
+++ 06-10-2022/output/resume/resume.js
@@ -0,0 +1,111 @@
+let resume={
+    name:' ',
+    mobile_no:8489063830,
+    email:'alfiyashalini@gmail.com',
+    role:'Full-Stack Developer',
+    Objective:"Obtaining a challenging position where I can utilize my subject knowledge, communications and technical skills for the development of the organization to the best of my ability",
+
+
+    education:[
+
+        {
+            course:'SSLC',
+            stream:10,
+            institute:'L.m.S school',
+            percentage:"86%",
+            year_of_passing:2014,
+        },
+        {
+            course:'HSC',
+            stream:12,
+            institute:'L.m.S school',
+            percentage:"75%",
+            year_of_passing:2016,
+        },
+        {
+            course:'BCA',
+            stream:'Computer Application',
+            institute:'MSU constituent college',
+            percentage:7.95,
+            year_of_passing:2019,
+        },
+        {
+            course:'MCA',
+            stream:'Computer Application',
+            institute:'LJCET college',
+            percentage:8.98,
+            year_of_passing:2021,
+        },
+    
+    ],
+    certification:[
+        {
+            course_name:" ",
+            duration:" ",
+            institute:" ",
+
+        }
+    ],
+    project:[
+        {
+            title:"  ",
+            obstract:" ",
+            
+            duration:" ",
+        }
+    ],
+    personal_details:[
+        
+        {
+            father_name:'ROBINSON',
+            mother_name:'SAHILA',
+            address:'14-82/1,seynamvilai,bethelpuram(p.o)',
+            DOB:05-04-1999,
+            gender:'female',
+            material_status:'single',
+            nationality: 'Indian',
+	        religion: 'Christian',
+        },
+    ],
+    language_known:["Tamil","English"],
+    hobbies:["music","drawing","gardening"],
+    skill:["HTML","CSS","JS"],
+    declaration:"I hereby declare that the above given information are true to the best of my knowledge and belief",
+    place:"seynamvilai",
+    date:" ",
+    signature:"  ",
+    
+}
+console.log(resume);
+let Resume={
+    skill:[],
+    education:[],
+    certification:[],
+    project:[],
+    personal_details:[],
+    
+    hobbies:[],
+    
+};
+
+function addDetails(key,value,index=null,indexkey=null){
+  if(index==null && indexkey==null){  
+    Resume[key]=value;
+  }
+  else if (indexkey==null){
+    Resume[key][index]=value;
+  }
+  else {
+        if(!Resume[key][index]){
+            Resume[key][index]={}
+        }
+        Resume[key][index][indexkey]=value;
+    }
+    display();
+
+}
+function display(){
+    document.getElementById("obj_data").innerHTML=JSON.stringify(Resume);
+}
+
+
